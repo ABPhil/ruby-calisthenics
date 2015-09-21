4173cc615083e0aefd104f3d82bc592e3c2ccb2a
abphil <andrewphillipsb@gmail.com>
2015-09-21 02:44:23 +0000

    finished attr accessor and fun with words -1 test

--- lib/fun_with_strings.rb
+++ lib/fun_with_strings.rb
@@ -1,15 +1,26 @@
 module FunWithStrings
   def palindrome?
-    # your code here
+    self.downcase.gsub(/[^a-z]/,'') == self.reverse.downcase.gsub(/[^a-z]/,'')
   end
   def count_words
-    # your code here
+    words = self.gsub(/[^[:word:]\s]/, '').split(' ')
+    frequency = Hash.new(0)
+    words.each { |word| frequency[word.downcase] += 1 }
+    return frequency
   end
   def anagram_groups
-    # your code here
+    arr = []
+    h = Hash.new(0)
+    self.split(' ').each do |word|
+    h.merge!(word.chars.sort word)
+    end
+  
+  h.each_key do |group|
+    arr << group
+  end
+  arr
   end
 end
-
 # make all the above functions available as instance methods on Strings:
 
 class String
